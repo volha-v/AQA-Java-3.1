1d19e42d941cd88b9c29b82e72b17163ea6a383c
volha-v <mawmaw@tut.by>
2025-06-25 21:51:38 +0300

    edit GitExampleFile

--- src/main/java/GitExampleFile.java
+++ src/main/java/GitExampleFile.java
@@ -1,5 +1,11 @@
 public class GitExampleFile {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+
+        int x = 0;
+        System.out.println(++x);
+
+
     }
 }
