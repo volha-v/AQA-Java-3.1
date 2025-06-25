51fbd55708381afdab176454060acfac1c48bfb8
volha-v <77641301+volha-v@users.noreply.github.com>
2025-06-25 22:05:13 +0300

    Update GitExampleFile.java

--- src/main/java/GitExampleFile.java
+++ src/main/java/GitExampleFile.java
@@ -2,7 +2,7 @@ public class GitExampleFile {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-
+// add some new comment
         int x = 0;
         System.out.println(++x);
 
